892e0e5576b03a3929b29cf7f976286dc9ea8edf
Ricardo Coelho <coelho.289@gmail.com>
2025-03-25 18:45:31 -0300

    Alterando limite para 20

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSoretados = [];
-let numeroLimite = 20;
+let numeroLimite = 25;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag,texto){
 }
 function exibirMensagemInicial(){
     exibirTextoNaTela ('h1', 'Jogo do número secreto');
-    exibirTextoNaTela ('p', 'Escolha um número entre 1 e 20' );
+    exibirTextoNaTela ('p', 'Escolha um número entre 1 e 25' );
 }
 exibirMensagemInicial();
 
